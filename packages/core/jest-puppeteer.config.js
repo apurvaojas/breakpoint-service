c5d0a15c15d06d0fdf3fafe9627a4c24f3b28d79
Apurva Ojas <apurvaojas10@gmail.com>
2020-04-06 10:49:02 +0530

    ci(pupprteer): puppeteer config fixes

--- packages/core/jest-puppeteer.config.js
+++ packages/core/jest-puppeteer.config.js
@@ -3,6 +3,12 @@ module.exports = {
     dumpio: true,
     headless: true,
     args: ['--disable-infobars'],
+    defaultViewport: {
+      width: 800,
+      height: 800,
+      isMobile: false,
+      hasTouch: false
+    }
   },
   browserContext: 'default',
 };
