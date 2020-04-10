63ea02e92b29083b3b22a38b9fb672fd37f5adc4
Apurva Ojas <apurvaojas10@gmail.com>
2020-04-10 19:44:08 +0530

    ci(puppeteer): test fixes

--- packages/core/jest-puppeteer.config.js
+++ packages/core/jest-puppeteer.config.js
@@ -1,14 +1,21 @@
-module.exports = {
+const conf = {
   launch: {
     dumpio: true,
-    headless: true,
+    headless: false,
     args: ['--disable-infobars'],
     defaultViewport: {
       width: 800,
       height: 800,
       isMobile: false,
       hasTouch: false
-    }
+    },
   },
   browserContext: 'default',
 };
+
+// if(process.platform === "win32" || process.platform === "darwin") {
+  // console.log("*******************************\n*******************************************\n\n")
+  // conf.launch.executablePath = '/usr/bin/google-chrome-stable';
+// }
+
+module.exports = conf;
